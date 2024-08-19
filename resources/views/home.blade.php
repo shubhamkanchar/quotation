c7c77167dd2b9f6d4c99ac1ac6717527d44d0ba1
shubhamkanchar <shubhamkanchar687@gmail.com>
2024-08-19 19:19:37 +0530

    dashboard and logo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,39 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                   <div class="row">
+                        <div class="offset-md-2 col-md-4">
+                            <a href="{{ route('business.index') }}"><div class="card mt-2">
+                                <div class="card-body">Business</div>
+                            </div></a>
+                            <a href="{{ route('customer.index') }}"><div class="card mt-2">
+                                <div class="card-body">Customer</div>
+                            </div></a>
+                            <a href="{{ route('product.index') }}"><div class="card mt-2">
+                                <div class="card-body">Product</div>
+                            </div></a>
+                            <a href="{{ route('term.index') }}"><div class="card mt-2">
+                                <div class="card-body">Terms</div>
+                            </div></a>
+                        </div>
+                        <div class="col-md-4">
+                            <a href="{{ route('make-quotation.create') }}"><div class="card mt-2">
+                                <div class="card-body">Make Quotation</div>
+                            </div></a>
+                            <a href="{{ route('make-invoice.create') }}"><div class="card mt-2">
+                                <div class="card-body">Make Invoice</div>
+                            </div></a>
+                            <a href="{{ route('make-purchase-order.create') }}"><div class="card mt-2">
+                                <div class="card-body">Make purchase order</div>
+                            </div></a>
+                            <a href="{{ route('make-proforma-invoice.create') }}"><div class="card mt-2">
+                                <div class="card-body">Make proforma invoice</div>
+                            </div></a>
+                            <a href="{{ route('make-delivery-note.create') }}"><div class="card mt-2">
+                                <div class="card-body">Make delivery notes</div>
+                            </div></a>
+                        </div>
+                   </div>
                 </div>
             </div>
         </div>
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -27,7 +27,8 @@
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{-- {{ config('app.name', 'Laravel') }} --}}
+                    <img style="width:150px" src='{{ asset('logo/Maharaja_.png') }}'>
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
